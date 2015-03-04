8e6d95cf5a66ba3aabe7170267bdcacfc175a39f
Paul Czarkowski <username.taken@gmail.com>
2015-03-04 15:07:46 -0600

    do I even english?

--- ursula_cli/shell.py
+++ ursula_cli/shell.py
@@ -236,9 +236,9 @@ def main():
     parser.add_argument('--vagrant', action='store_true',
                         help='Provision environment in vagrant')
     parser.add_argument('--pre',
-                        help='comma seperate list of playbooks to run before')
+                        help='comma seperated list of playbooks to run before')
     parser.add_argument('--post',
-                        help='comma seperate list of playbooks to run after')
+                        help='comma seperated list of playbooks to run after')
 
     args, extra_args = parser.parse_known_args()
 
